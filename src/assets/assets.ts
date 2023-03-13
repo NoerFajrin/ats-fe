2d25ad6d2d91d9ddd687e3782f45a3a44fb274b5
alqinsidev <padlanalqinsi@live.com>
2023-03-13 15:07:30 +0700

    add(imageHandler): general

---
+++ src/assets/assets.ts
@@ -0,0 +1,11 @@
+import lambang_korlantas from './lambang_korlantas.png'
+import lambang_polri from './lambang_polri.png'
+
+const assets = {
+    images: {
+        lambang_korlantas,
+        lambang_polri
+    }
+}
+
+export default assets
