2fc97afec2ea96ba26fa0fb8249a1367eae3afa7
NoerFajrin <frinrin23@gmail.com>
2023-03-14 10:36:27 +0700

    logo dan menu

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,56 +1,79 @@
 import React from 'react';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
+import { DashboardOutlined, FormOutlined, LaptopOutlined, NotificationOutlined,MailOutlined, UserOutlined } from '@ant-design/icons';
+import { Image, MenuProps, Space, Typography } from 'antd';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import assets from '../../assets/assets';
 
-const { Header, Content, Sider } = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 
-const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
-
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
-
-      children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1;
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        };
-      }),
-    };
+const items2 :MenuProps['items'] = [
+  {
+    key: `tugas_aktif`,
+    icon: React.createElement(DashboardOutlined),
+    label: <Link to="/tugas-aktif">Tugas Aktif</Link> 
   },
-);
-
+  {
+    key: `surat_tugas`,
+    icon: React.createElement(MailOutlined),
+    label: <Link to="/surat-tugas">Surat Tugas</Link> 
+  },
+  {
+    key: `penugasan`,
+    icon: React.createElement(FormOutlined),
+    label: <Link to="/surat-tugas">Penugasan</Link> 
+  },
+  {
+    key: `field_personel`,
+    icon: React.createElement(UserOutlined),
+    label: <Link to="/field-personel">Field Personel</Link> 
+  },
+]
 const AdminLayout: React.FC = () => {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
 
   return (
-    <Layout style={{minHeight:'100vh'}}>
-      <Header className="header">
-        <div className="logo" />
-      </Header>
-      <Layout>
-        <Sider width={200} style={{ background: colorBgContainer }}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%', borderRight: 0 }}
-            items={items2}
-          />
-        </Sider>
-        <Layout style={{ padding: '0 24px 24px' }}>
-          <Outlet/>
-        </Layout>
-      </Layout>
+    <Layout hasSider>
+    <Sider
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        bottom: 0,
+      }}
+    >
+      <Space direction="horizontal" size="middle" style={{ display: 'flex', paddingLeft:2,paddingTop:4}}>
+       <img
+         height={45} src={assets.images.lambang_korlantas}
+       />
+       <Typography.Text style={{color:'#f6ffed'}}>ATS</Typography.Text>
+      </Space>
+      {/* <div style={{ height: 32, margin: 16, }} /> */}
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items2} />
+    </Sider>
+    <Layout className="site-layout" style={{ marginLeft: 200 }}>
+      <Header style={{ padding: 0, background: colorBgContainer }} />
+      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+        <div style={{ padding: 24, textAlign: 'center', background: colorBgContainer }}>
+          <p>long content</p>
+          {
+            // indicates very long content
+            Array.from({ length: 100 }, (_, index) => (
+              <React.Fragment key={index}>
+                {index % 20 === 0 && index ? 'more' : '...'}
+                <br />
+              </React.Fragment>
+            ))
+          }
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
     </Layout>
+  </Layout>
   );
 };
 
