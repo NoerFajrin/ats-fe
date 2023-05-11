1ea08a4e74d66d882f440864cece9042d7279860
NoerFajrin <frinrin23@gmail.com>
2023-05-11 20:00:14 +0700

    useformik

--- src/res/api/client.ts
+++ src/res/api/client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-    // baseURL:'http://10.10.3.1:2222'
-    baseURL:`http://192.168.18.3:2222`
+    baseURL:'http://10.10.3.1:2222'
+    // baseURL:`http://192.168.18.3:2222`
     })
 
 export default client
